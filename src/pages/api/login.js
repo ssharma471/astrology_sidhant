764fc0a6c77a0293986475a6dd23c3da80a0678a
Meetsimar <meetsimark@gmail.com>
2024-02-20 21:21:48 -0500

    Fixed login in the api routes

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,23 +1,25 @@
-// pages/api/register.js
-const {
-    connect,
-    checkUser,
-  } = require("../../../user-api/mongodb"); // Import mongodb.js functions
-  
-  export default async function handler(req, res) {
-    if (req.method === "POST") {
-      try {
-        await connect()
-        const { email, password } = req.body;
-        await checkUser({ email, password });
-        res.status(201).json({ message: "User logged in successfully" });
-      } catch (error) {
-        console.error("Error loggin in user:", error);
-        res.status(500).json({ error: "Internal server error" });
+// pages/api/login.js
+const { connect, checkUser } = require("../../../user-api/mongodb"); // Import mongodb.js functions
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      await connect();
+      const { email, password } = req.body;
+      const user = await checkUser(email, password);
+      if (user) {
+        res.json({ success: true });
+      } else {
+        res
+          .status(401)
+          .json({ success: false, message: "Invalid email or password" });
       }
-    }else{
-      res.setHeader('Allow', ['POST']);
-      res.status(405).end(`Method ${req.method} Not Allowed`);
+    } catch (error) {
+      console.log("Error loggin in user:", error);
+      res.status(500).json({ error: "Internal server error" });
     }
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
-  
+}
