59e311a5762b79764764e58756cb116475f6c8eb
Samarth-Modi <77992497+Samarth-Modi@users.noreply.github.com>
2024-04-10 00:30:08 -0400

    Update userProfile.js

--- src/pages/userProfile.js
+++ src/pages/userProfile.js
@@ -1,16 +1,14 @@
 // src/pages/UserProfile.js
 
 import React from 'react';
-import PrivateRoute from 'components/PrivateRoute'; // Import PrivateRoute
+
 
 const UserProfile = () => {
   return (
-    <PrivateRoute>
       <div>
         <h2>User Profile</h2>
         {/* Your profile content goes here */}
       </div>
-    </PrivateRoute>
   );
 };
 
